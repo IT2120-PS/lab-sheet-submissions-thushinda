aaff0d6ba78aeba8a94e16fb7993a0ec00789617
IT24101086 <it24101086@my.sliit.lk>
2025-09-23 01:28:23 +0530

    Lab 07

---
+++ Lab Sheet 07/IT24101086.R
@@ -0,0 +1,10 @@
+setwd("D:\\Y2 S1\\Probability and Statistics - IT2120\\Labs\\week 7")
+getwd()
+
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+pexp(2, rate = 1/3)
+
+1 - pnorm(130, mean = 100, sd = 15)
+
+qnorm(0.95, mean = 100, sd = 15)
